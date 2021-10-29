baaeb7dfc7550a72ceff2b20b2b40e6bc52d1769
Ryan Arana <ryan.arana@hey.com>
2021-10-29 13:55:55 -0700

    Add empty project for 2020

---
+++ advent-of-code-2020/advent-of-code-2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  advent-of-code-2020
+//
+//  Created by Ryan Arana on 10/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ advent-of-code-2020/advent-of-code-2020/advent_of_code_2020App.swift
@@ -0,0 +1,17 @@
+//
+//  advent_of_code_2020App.swift
+//  advent-of-code-2020
+//
+//  Created by Ryan Arana on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct advent_of_code_2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
