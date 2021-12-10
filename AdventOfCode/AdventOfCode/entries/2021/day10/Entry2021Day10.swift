e88345ba3556712da83bba820de6c9eb817608ce
Ryan Arana <ryan.arana@hey.com>
2021-12-09 16:14:35 -0800

    Add 2021 Day 9 files

---
+++ AdventOfCode/AdventOfCode/entries/2021/day10/Entry2021Day10.swift
@@ -0,0 +1,31 @@
+//
+//  Entry2021Day10.swift
+//  AdventOfCode
+//
+//  Created by Ryan Arana on 12/9/21.
+//
+
+import Foundation
+
+class Entry2021Day10: Entry {
+    init(_ part: Part) {
+        super.init(year: 2021, day: 10, part: part)
+    }
+
+    @Sendable override func run() async throws {
+        let inputs = try String(contentsOf: try fileURL())
+            .split(separator: "\n")
+
+        let answer = await run(for: inputs)
+        DispatchQueue.main.async {
+            self.answer = "\(answer)"
+        }
+    }
+
+    func run<AnyString: StringProtocol>(for input: [AnyString]) async -> Int {
+        progress.totalUnitCount = Int64(input.count)
+        defer { progress.completedUnitCount = progress.totalUnitCount }
+
+        return 0
+    }
+}
