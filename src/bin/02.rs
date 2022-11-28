cd9c5da012eb844ac9ade3a721783e5bec3c1c78
Ryan Arana <ryan.arana@hey.com>
2022-11-27 19:31:54 -0800

    Solve day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -21,7 +21,27 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let result = input
+        .lines()
+        .fold((0, 0, 0), |(x, y, a), line| {
+            let (cmd, v) = line
+                .split_whitespace()
+                .collect_tuple()
+                .unwrap();
+            let amount: isize = v.parse().unwrap();
+            match cmd {
+                "down" => (x, y, a + amount),
+                "up" =>  (x, y, a - amount),
+                "forward" => (
+                    x + amount,
+                    y + (a * amount),
+                    a
+                ),
+                _ => (0, 0, 0),
+            }
+        });
+
+    Some(result.0 as u32 * result.1 as u32)
 }
 
 fn main() {
@@ -43,6 +63,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = aoc::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(900));
     }
 }
