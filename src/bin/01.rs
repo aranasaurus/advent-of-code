b8b20fa226bc7c18a5c3689a4e0d8454f10057ff
Ryan Arana <ryan.arana@hey.com>
2022-11-27 16:55:58 -0800

    Solve day 1 part 2
    Refactor part 1 to use the same, functional approach.

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,24 +1,25 @@
+use itertools::Itertools;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut prev: Option<u32> = None;
-    let mut count: u32 = 0;
-    for number in input.split_whitespace().filter_map(|x| x.parse().ok()) {
-        count += match prev {
-            None => {
-                prev = Some(number);
-                0
-            },
-            Some(previous) => {
-                let result = if number > previous { 1 } else { 0 };
-                prev = Some(number);
-                result
-            }
-        }
-    }
-    Some(count)
+    let result = input
+        .lines()
+        .map(|line| { line.parse::<u16>().unwrap() })
+        .tuple_windows()
+        .filter(|(a, b)| a < b )
+        .count();
+    
+    Some(result as u32)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let result = input
+        .lines()
+        .map(|line| { line.parse::<u16>().unwrap() })
+        .tuple_windows()
+        .filter(|(a, _, _, b)| a < b )
+        .count();
+    
+    Some(result as u32)
 }
 
 fn main() {
@@ -40,6 +41,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = aoc::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(5));
     }
 }
