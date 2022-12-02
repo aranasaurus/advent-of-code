3d432d509f784899261bb1a9669337226d3fb815
Ryan Arana <ryan.arana@hey.com>
2022-12-01 22:33:34 -0800

    Solve day 2 part 1

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,8 +1,28 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+fn score_round(round: &str) -> u32 {
+    let players: Vec<i8> = round
+        .split_whitespace()
+        .map(|s| match s {
+            "A" | "X" => 1,
+            "B" | "Y" => 2,
+            "C" | "Z" => 3,
+            _ => unreachable!(),
+        })
+        .collect();
+
+    let result = (players[1] - players[0]).rem_euclid(3);
+    (match result {
+        0 => players[1] + 3, // draw
+        1 => players[1] + 6, // win
+        2 => players[1],     // lose
+        _ => unreachable!(),
+    }) as u32
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(input.lines().map(score_round).sum())
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
