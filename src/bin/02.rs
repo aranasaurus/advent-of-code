f902481ac79c60727e72c8972328e427ec3e7998
Ryan Arana <ryan.arana@hey.com>
2022-12-02 00:18:33 -0800

    Solve day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,29 +1,65 @@
-fn score_round(round: &str) -> u32 {
-    let players: Vec<i8> = round
-        .split_whitespace()
-        .map(|s| match s {
-            "A" | "X" => 1,
-            "B" | "Y" => 2,
-            "C" | "Z" => 3,
-            _ => unreachable!(),
-        })
-        .collect();
-
-    let result = (players[1] - players[0]).rem_euclid(3);
+fn score_round(opponent: i8, player: i8) -> u32 {
+    let result = (player - opponent).rem_euclid(3);
     (match result {
-        0 => players[1] + 3, // draw
-        1 => players[1] + 6, // win
-        2 => players[1],     // lose
+        0 => player + 3, // draw
+        1 => player + 6, // win
+        2 => player,     // lose
         _ => unreachable!(),
     }) as u32
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    Some(input.lines().map(score_round).sum())
+fn letter_to_player(letter: &str) -> i8 {
+    match letter.trim() {
+        "A" | "X" => 1,
+        "B" | "Y" => 2,
+        "C" | "Z" => 3,
+        _ => unreachable!(),
+    }
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let (opp_str, player_str) = line.split_at(1);
+                score_round(letter_to_player(opp_str), letter_to_player(player_str))
+            })
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let (opp_str, strat) = line.split_at(1);
+                let opponent = letter_to_player(opp_str);
+                // there's definitely a smarter way to do this, but I spent an hour on it and I give up for tonight!
+                let player = match strat.trim() {
+                    "X" => match opponent {
+                        // lose
+                        1 => 3,
+                        2 => 1,
+                        3 => 2,
+                        _ => unreachable!(),
+                    },
+                    "Y" => opponent, // draw
+                    "Z" => match opponent {
+                        // win
+                        1 => 2,
+                        2 => 3,
+                        3 => 1,
+                        _ => unreachable!(),
+                    },
+                    _ => unreachable!(),
+                };
+
+                score_round(opponent, player)
+            })
+            .sum(),
+    )
 }
 
 fn main() {
@@ -45,6 +81,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
