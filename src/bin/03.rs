372b088a40cea16882527bce064a4e1cce6033f2
Ryan Arana <ryan.arana@hey.com>
2022-12-02 23:50:35 -0800

    Solve day 3 part 1

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,5 +1,31 @@
+fn priority(item: char) -> u8 {
+    if item.is_ascii_lowercase() {
+        1 + item as u8 - b'a'
+    } else {
+        27 + item as u8 - b'A'
+    }
+}
+
+fn find_dup(sack: &str) -> char {
+    let (left, right) = sack.split_at(sack.len() / 2);
+    for l in left.chars() {
+        for r in right.chars() {
+            if l == r {
+                return l;
+            }
+        }
+    }
+
+    panic!("No duplicate found")
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    Some(
+        input
+            .lines()
+            .map(|line| priority(find_dup(line)) as u32)
+            .sum(),
+    )
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
