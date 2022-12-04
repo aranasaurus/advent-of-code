77ed5e879bd9cd057b81f7cb8959a6e9c999ca5b
Ryan Arana <ryan.arana@hey.com>
2022-12-03 23:12:24 -0800

    Refactor day 04 to get rid of duplicate code
    These changes seem to make it a little bit slower (10-20us or so), but I really hate copy/pasta

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -5,23 +5,10 @@ pub fn part_one(input: &str) -> Option<u32> {
         input
             .lines()
             .filter(|line| {
-                let (left, right) = line.split_once(',').unwrap();
-                let (left_start, left_end) = left
-                    .split_terminator('-')
-                    .map(|item| item.parse::<u32>().unwrap())
-                    .collect_tuple()
-                    .unwrap();
-                let (right_start, right_end) = right
-                    .split_terminator('-')
-                    .map(|item| item.parse::<u32>().unwrap())
-                    .collect_tuple()
-                    .unwrap();
+                let (left, right) = parse_ranges(line);
 
-                let left_range = left_start..=left_end;
-                let right_range = right_start..=right_end;
-
-                (right_range.contains(&left_start) && right_range.contains(&left_end))
-                    || (left_range.contains(&right_start) && left_range.contains(&right_end))
+                (right.contains(left.start()) && right.contains(left.end()))
+                    || (left.contains(right.start()) && left.contains(right.end()))
             })
             .count() as u32,
     )
@@ -32,28 +19,33 @@ pub fn part_two(input: &str) -> Option<u32> {
         input
             .lines()
             .filter(|line| {
-                let (left, right) = line.split_once(',').unwrap();
-                let (left_start, left_end) = left
-                    .split_terminator('-')
-                    .map(|item| item.parse::<u32>().unwrap())
-                    .collect_tuple()
-                    .unwrap();
-                let (right_start, right_end) = right
-                    .split_terminator('-')
-                    .map(|item| item.parse::<u32>().unwrap())
-                    .collect_tuple()
-                    .unwrap();
+                let (left, right) = parse_ranges(line);
 
-                let left_range = left_start..=left_end;
-                let right_range = right_start..=right_end;
-
-                right_range.contains(&left_start)
-                    || right_range.contains(&left_end)
-                    || left_range.contains(&right_start)
-                    || left_range.contains(&right_end)
+                right.contains(left.start())
+                    || right.contains(left.end())
+                    || left.contains(right.start())
+                    || left.contains(right.end())
             })
             .count() as u32,
     )
+}
+
+fn parse_ranges(line: &&str) -> (std::ops::RangeInclusive<u32>, std::ops::RangeInclusive<u32>) {
+    let (left, right) = line.split_once(',').unwrap();
+
+    let (left_start, left_end) = parse_range(left);
+    let (right_start, right_end) = parse_range(right);
+
+    (left_start..=left_end, right_start..=right_end)
+}
+
+fn parse_range(range: &str) -> (u32, u32) {
+    let (start, end) = range
+        .split_terminator('-')
+        .map(|item| item.parse::<u32>().unwrap())
+        .collect_tuple()
+        .unwrap();
+    (start, end)
 }
 
 fn main() {
