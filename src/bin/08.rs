b3293a28c7f206c43f1d68052230f072323a1742
Ryan Arana <ryan.arana@hey.com>
2022-12-10 14:17:29 -0800

    Solve day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,4 +1,7 @@
-use itertools::Itertools;
+use itertools::{
+    FoldWhile::{Continue, Done},
+    Itertools,
+};
 
 fn parse_heights(input: &str) -> Vec<Vec<u8>> {
     input
@@ -9,6 +12,18 @@ fn parse_heights(input: &str) -> Vec<Vec<u8>> {
                 .collect::<Vec<u8>>()
         })
         .collect::<Vec<Vec<u8>>>()
+}
+
+fn check_blockage(
+    visible_tree: u8,
+    target_tree: u8,
+    current_score: u32,
+) -> itertools::FoldWhile<u32> {
+    if visible_tree >= target_tree {
+        Done(current_score + 1)
+    } else {
+        Continue(current_score + 1)
+    }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -71,7 +86,48 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let heights = parse_heights(input);
+    let h = heights.len();
+    let w = heights[0].len();
+    let mut scores = vec![vec![0_u32; w]; h];
+
+    for y in 1..heights.len() - 1 {
+        for x in 1..heights[y].len() - 1 {
+            let column = heights.iter().map(|row| row[x]).collect_vec();
+            let row = &heights[y];
+            let tree = row[x];
+
+            // up
+            let mut score = column[..y]
+                .iter()
+                .rev()
+                .fold_while(0, |acc, &t| check_blockage(t, tree, acc))
+                .into_inner();
+
+            // down
+            score *= column[y + 1..]
+                .iter()
+                .fold_while(0, |acc, &t| check_blockage(t, tree, acc))
+                .into_inner();
+
+            // left
+            score *= row[..x]
+                .iter()
+                .rev()
+                .fold_while(0, |acc, &t| check_blockage(t, tree, acc))
+                .into_inner();
+
+            // right
+            score *= row[x + 1..]
+                .iter()
+                .fold_while(0, |acc, &t| check_blockage(t, tree, acc))
+                .into_inner();
+
+            scores[y][x] = score;
+        }
+    }
+
+    Some(*scores.iter().flatten().max().unwrap())
 }
 
 fn main() {
@@ -93,7 +149,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 8);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(8));
     }
 
     #[test]
@@ -101,6 +157,6 @@ mod tests {
     fn test_solutions() {
         let input = advent_of_code::read_file("inputs", 8);
         assert_eq!(part_one(&input), Some(1733));
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(284648));
     }
 }
