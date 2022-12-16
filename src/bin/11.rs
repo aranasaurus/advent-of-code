7b581bf6b79264ee2b48c4b3f37af9ce2871e601
Ryan Arana <ryan.arana@hey.com>
2022-12-15 23:32:56 -0800

    Solve day 11 part 2
    I totally had to find help with the math trick :P
    
    Multiply all the divisible_by's together and then mod the worry levels with that before doing any math on them. I called it LCM for Lowest Common Multiple... which is not what it _is_, it's what it helps you find, I think?

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -13,22 +13,27 @@ use nom::{
 
 #[derive(Debug, Clone)]
 struct Monkey {
-    items: VecDeque<u32>,
-    inspections: u32,
+    items: VecDeque<u64>,
+    inspections: u64,
     operation: Operation,
-    divisible_by: u32,
+    divisible_by: u64,
     true_target: usize,
     false_target: usize,
 }
 
 impl Monkey {
-    fn inspect(&mut self) -> (usize, u32) {
-        let item = self.items.pop_front().unwrap();
-        let worry = match self.operation {
+    fn inspect(&mut self, reduce_worry: bool, lcm: u64) -> (usize, u64) {
+        let item = self.items.pop_front().unwrap() % lcm;
+        let mut worry = match self.operation {
             Operation::Multiply(value) => item * value,
             Operation::Add(value) => item + value,
             Operation::Square => item * item,
-        } / 3;
+        };
+
+        if reduce_worry {
+            worry /= 3;
+        }
+
         self.inspections += 1;
 
         let result = worry % self.divisible_by == 0;
@@ -45,8 +50,8 @@ impl Monkey {
 
 #[derive(Debug, Clone)]
 enum Operation {
-    Multiply(u32),
-    Add(u32),
+    Multiply(u64),
+    Add(u64),
     Square,
 }
 
@@ -59,7 +64,7 @@ fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
     let (input, (_, items)) = separated_pair(
         tag("Starting items:"),
         space1,
-        separated_list1(tag(", "), nom::character::complete::u32),
+        separated_list1(tag(", "), nom::character::complete::u64),
     )(input)?;
 
     // Operation
@@ -83,15 +88,15 @@ fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
     // Test
     let (input, _) = multispace1(input)?;
     let (input, _) = tag("Test: divisible by ")(input)?;
-    let (input, divisor) = nom::character::complete::u32(input)?;
+    let (input, divisor) = nom::character::complete::u64(input)?;
     let (input, _) = multispace1(input)?;
     let (input, _) = tag("If true: throw to monkey ")(input)?;
     let (input, true_monkey) =
-        nom::combinator::map(nom::character::complete::u32, |i| i as usize)(input)?;
+        nom::combinator::map(nom::character::complete::u64, |i| i as usize)(input)?;
     let (input, _) = multispace1(input)?;
     let (input, _) = tag("If false: throw to monkey ")(input)?;
     let (input, false_monkey) =
-        nom::combinator::map(nom::character::complete::u32, |i| i as usize)(input)?;
+        nom::combinator::map(nom::character::complete::u64, |i| i as usize)(input)?;
 
     // Eat the line_ending if it's there
     let (input, _) = opt(line_ending)(input)?;
@@ -108,13 +113,14 @@ fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
     ))
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let (_, mut monkeys) = separated_list1(newline, parse_monkey)(input).unwrap();
+    let lcm: u64 = monkeys.iter().map(|m| m.divisible_by).product();
     for _ in 0..20 {
         for i in 0..monkeys.len() {
             for _ in 0..monkeys[i].items.len() {
                 let monkey = monkeys.get_mut(i).unwrap();
-                let (target_index, item) = monkey.inspect();
+                let (target_index, item) = monkey.inspect(true, lcm);
                 monkeys.get_mut(target_index).unwrap().items.push_back(item);
             }
         }
@@ -131,8 +137,28 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let (_, mut monkeys) = separated_list1(newline, parse_monkey)(input).unwrap();
+    let lcm: u64 = monkeys.iter().map(|m| m.divisible_by).product();
+    for _ in 0..10000 {
+        for i in 0..monkeys.len() {
+            for _ in 0..monkeys[i].items.len() {
+                let monkey = monkeys.get_mut(i).unwrap();
+                let (target_index, item) = monkey.inspect(false, lcm);
+                monkeys.get_mut(target_index).unwrap().items.push_back(item);
+            }
+        }
+    }
+    Some(
+        monkeys
+            .iter()
+            .map(|m| m.inspections)
+            .sorted()
+            .rev()
+            .collect_vec()[..=1]
+            .iter()
+            .product(),
+    )
 }
 
 fn main() {
@@ -154,7 +180,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 11);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(2713310158));
     }
 
     #[test]
@@ -162,7 +188,7 @@ mod tests {
     fn test_solutions() {
         let input = advent_of_code::read_file("inputs", 11);
         assert_eq!(part_one(&input), Some(56350));
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(13954061248));
     }
 
     #[test]
@@ -176,12 +202,12 @@ mod tests {
             false_target: 1,
         };
 
-        assert_eq!(m1.inspect(), (2, 3));
+        assert_eq!(m1.inspect(true, 3000), (2, 3));
         assert_eq!(m1.inspections, 1);
         assert_eq!(m1.items.is_empty(), true);
 
         m1.items.push_front(7);
-        assert_eq!(m1.inspect(), (1, 4));
+        assert_eq!(m1.inspect(true, 3000), (1, 4));
         assert_eq!(m1.inspections, 2);
         assert_eq!(m1.items.is_empty(), true);
     }
