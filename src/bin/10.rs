95c078c392895fce35c7c7a8af89133efe888cf9
Ryan Arana <ryan.arana@hey.com>
2022-12-15 16:17:40 -0800

    Solve day 10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use nom::{
     character::complete::{alpha1, newline, space1},
     error::ErrorKind,
@@ -28,29 +29,31 @@ fn parse_operation(input: &str) -> IResult<&str, Operation> {
     }
 }
 
-pub fn part_one(input: &str) -> Option<i32> {
-    let (_, operations) = separated_list1(newline, parse_operation)(input).unwrap();
+fn run(operations: Vec<Operation>) -> Vec<i32> {
     let mut x = 1;
     let mut x_history = vec![x];
-
-    for op in operations {
-        match op {
-            Operation::Add(value) => {
-                x_history.push(x);
-                x_history.push(x);
-                x += value;
-            }
-            Operation::Noop => {
-                x_history.push(x);
-            }
+    operations.into_iter().for_each(|op| match op {
+        Operation::Add(value) => {
+            x_history.push(x);
+            x_history.push(x);
+            x += value;
         }
-    }
+        Operation::Noop => {
+            x_history.push(x);
+        }
+    });
+    x_history
+}
+
+pub fn part_one(input: &str) -> Option<i32> {
+    let (_, operations) = separated_list1(newline, parse_operation)(input).unwrap();
+    let x_history = run(operations);
 
     let interesting_cycles = vec![20_usize, 60, 100, 140, 180, 220];
 
     let sum: i32 = interesting_cycles
         .iter()
-        .filter(|&&c| c < x_history.len())
+        .filter(|&&c| c < x_history.len()) // this is to support the test that only checks against the first 20 cycles
         .map(|&c| -> i32 {
             let x = x_history[c];
             x * c as i32
@@ -60,8 +63,32 @@ pub fn part_one(input: &str) -> Option<i32> {
     Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<String> {
+    let (_, operations) = separated_list1(newline, parse_operation)(input).unwrap();
+    let x_history = run(operations);
+
+    let mut crt = vec!["."; 240];
+    x_history
+        .iter()
+        .dropping(1)
+        .enumerate()
+        .for_each(|(pixel, &x)| {
+            // crt is a 1d vec, storing all 6 rows of the display contiguously. Each row is 40 pixels.
+            // So h_pos is what would be the x component of a pixel's coordinate if we were using cartesian coordinates.
+            let h_pos = (pixel % 40) as i32;
+            if h_pos.abs_diff(x) <= 1 {
+                crt[pixel] = "#";
+            }
+        });
+
+    crt.insert(40, "\n");
+    crt.insert(81, "\n");
+    crt.insert(122, "\n");
+    crt.insert(163, "\n");
+    crt.insert(204, "\n");
+
+    let result = crt.concat();
+    Some(result)
 }
 
 fn main() {
@@ -82,7 +109,8 @@ mod tests {
 
     #[test]
     fn test_part_one_20() {
-        let input = "addx 15
+        let input = "
+addx 15
 addx -11
 addx 6
 addx -3
@@ -92,14 +120,28 @@ addx -8
 addx 13
 addx 4
 noop
-addx -1";
+addx -1"
+            .trim();
         assert_eq!(part_one(&input), Some(420));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 10);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(
+            part_two(&input),
+            Some(
+                "
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."
+                    .trim()
+                    .into()
+            )
+        );
     }
 
     #[test]
@@ -107,6 +149,19 @@ addx -1";
     fn test_solutions() {
         let input = advent_of_code::read_file("inputs", 10);
         assert_eq!(part_one(&input), Some(14360));
-        assert_eq!(part_two(&input), None);
+        assert_eq!(
+            part_two(&input),
+            Some(
+                "
+###...##..#..#..##..####.###..####.####.
+#..#.#..#.#.#..#..#.#....#..#.#.......#.
+###..#....##...#..#.###..#..#.###....#..
+#..#.#.##.#.#..####.#....###..#.....#...
+#..#.#..#.#.#..#..#.#....#.#..#....#....
+###...###.#..#.#..#.####.#..#.####.####."
+                    .trim()
+                    .into()
+            )
+        );
     }
 }
