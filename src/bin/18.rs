5e29a069cbe0336b136f2129c103cdb41d3356e2
Ryan Arana <ryan.arana@hey.com>
2022-12-23 20:42:54 -0800

    Solve day 18 part 2

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,20 +1,20 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 use nom::{
     bytes::complete::tag, character::complete::line_ending, multi::separated_list1,
     sequence::tuple, IResult,
 };
 
-type Int = u16;
+type Int = i16;
 type Point = (Int, Int, Int);
 
 fn parse_point(input: &str) -> IResult<&str, Point> {
     let (input, (x, _, y, _, z)) = tuple((
-        nom::character::complete::u16,
+        nom::character::complete::i16,
         tag(","),
-        nom::character::complete::u16,
+        nom::character::complete::i16,
         tag(","),
-        nom::character::complete::u16,
+        nom::character::complete::i16,
     ))(input)?;
 
     Ok((input, (x, y, z)))
@@ -25,23 +25,17 @@ fn surrounding_points(point: &Point) -> HashSet<Point> {
     // right
     points.insert((point.0 + 1, point.1, point.2));
     // left
-    if point.0 > 0 {
-        points.insert((point.0 - 1, point.1, point.2));
-    }
+    points.insert((point.0 - 1, point.1, point.2));
 
     // above
     points.insert((point.0, point.1 + 1, point.2));
     // below
-    if point.1 > 0 {
-        points.insert((point.0, point.1 - 1, point.2));
-    }
+    points.insert((point.0, point.1 - 1, point.2));
 
     // back
     points.insert((point.0, point.1, point.2 + 1));
     // front
-    if point.2 > 0 {
-        points.insert((point.0, point.1, point.2 - 1));
-    }
+    points.insert((point.0, point.1, point.2 - 1));
 
     points
 }
@@ -53,24 +47,55 @@ pub fn part_one(input: &str) -> Option<u32> {
     let result = points.iter().fold(points.len() * 6, |acc, point| {
         let adjacent = surrounding_points(point);
         let existing = adjacent.difference(&points).count();
-        let mut edges = 0;
-        if point.0 == 0 {
-            edges += 1;
-        }
-        if point.1 == 0 {
-            edges += 1;
-        }
-        if point.2 == 0 {
-            edges += 1;
-        }
-        acc - (6 - existing) + edges
+        acc - (6 - existing)
     });
 
     Some(result as u32)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let (_, points_list) = separated_list1(line_ending, parse_point)(input).unwrap();
+
+    let points: HashSet<Point> = HashSet::from_iter(points_list.iter().cloned());
+
+    let max_x = points.iter().map(|p| p.0).max().unwrap();
+    let max_y = points.iter().map(|p| p.1).max().unwrap();
+    let max_z = points.iter().map(|p| p.2).max().unwrap();
+    let min_x = points.iter().map(|p| p.0).min().unwrap();
+    let min_y = points.iter().map(|p| p.1).min().unwrap();
+    let min_z = points.iter().map(|p| p.2).min().unwrap();
+    let x_range = min_x - 1..=max_x + 1;
+    let y_range = min_y - 1..=max_y + 1;
+    let z_range = min_z - 1..=max_z + 1;
+
+    let mut seen = HashSet::new();
+    let mut to_see = VecDeque::new();
+    to_see.push_back((*x_range.start(), *y_range.start(), *z_range.start()));
+
+    let mut sides = 0;
+    while let Some(coord) = to_see.pop_front() {
+        if !seen.insert(coord) {
+            continue;
+        }
+
+        let adjacents = surrounding_points(&coord);
+        for adjacent in adjacents {
+            if !x_range.contains(&adjacent.0)
+                || !y_range.contains(&adjacent.1)
+                || !z_range.contains(&adjacent.2)
+            {
+                continue;
+            }
+
+            if points.contains(&adjacent) {
+                sides += 1;
+            } else {
+                to_see.push_back(adjacent);
+            }
+        }
+    }
+
+    Some(sides as u32)
 }
 
 fn main() {
@@ -100,7 +125,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 18);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(58));
     }
 
     #[test]
@@ -108,6 +133,6 @@ mod tests {
     fn test_solutions() {
         let input = advent_of_code::read_file("inputs", 18);
         assert_eq!(part_one(&input), Some(4244));
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(2460));
     }
 }
