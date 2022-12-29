e29810aa5bf99c84b6de7bd50b838a6c366057a5
Ryan Arana <ryan.arana@hey.com>
2022-12-28 21:22:36 -0800

    Solve day 20 part 2
    I tried pulling the mix part into a function and calling it from both parts but it never works for part 2 and I can't see how the two implementations are different... movin on and just leaving it copy/pasta'd

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -6,36 +6,7 @@ fn parse_numbers(input: &str) -> IResult<&str, Vec<(usize, i64)>> {
     Ok((input, v.into_iter().enumerate().collect_vec()))
 }
 
-pub fn part_one(input: &str) -> Option<i64> {
-    let (_, numbers) = parse_numbers(input).unwrap();
-    // dbg!(&numbers);
-    let mut mixed = numbers.clone();
-
-    for n in numbers {
-        if let Some(i) = mixed.iter().position(|m| m.0 == n.0) {
-            // dbg!(n);
-            mixed.remove(i);
-            let (next_i, overflowed) = i.overflowing_add_signed(n.1 as isize);
-            let next_i = if overflowed {
-                let overflow_amount = usize::MAX - next_i;
-                if n.1.is_negative() {
-                    // mixed.len() - overflow_amount - 1
-                    if let Some(diff) = mixed.len().checked_sub(overflow_amount) {
-                        diff - 1
-                    } else {
-                        mixed.len() - overflow_amount.rem_euclid(mixed.len()) - 1
-                    }
-                } else {
-                    overflow_amount - 1
-                }
-            } else {
-                next_i.rem_euclid(mixed.len())
-            };
-            mixed.insert(next_i, n);
-            // dbg!(&mixed);
-        }
-    }
-
+fn coordinates(mixed: Vec<(usize, i64)>) -> Option<i64> {
     let mut result = vec![0i64; 3];
     if let Some(zero_i) = mixed.iter().position(|n| n.1 == 0) {
         result[0] = mixed[zero_i.wrapping_add(1000).rem_euclid(mixed.len())].1;
@@ -45,8 +16,37 @@ pub fn part_one(input: &str) -> Option<i64> {
     Some(result.iter().sum())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<i64> {
+    let (_, numbers) = parse_numbers(input).unwrap();
+
+    let mut mixed = numbers.clone();
+    for n in numbers {
+        if let Some(i) = mixed.iter().position(|m| m.0 == n.0) {
+            let removed = mixed.remove(i);
+            let next_i = (i as i64 + removed.1).rem_euclid(mixed.len() as i64);
+            mixed.insert(next_i as usize, removed);
+        }
+    }
+
+    coordinates(mixed)
+}
+
+pub fn part_two(input: &str) -> Option<i64> {
+    let (_, mut numbers) = parse_numbers(input).unwrap();
+    numbers.iter_mut().for_each(|n| n.1 *= 811589153);
+
+    let mut mixed = numbers.clone();
+    for _ in 0..10 {
+        for n in numbers.iter() {
+            if let Some(i) = mixed.iter().position(|m| m.0 == n.0) {
+                let removed = mixed.remove(i);
+                let next_i = (i as i64 + removed.1).rem_euclid(mixed.len() as i64);
+                mixed.insert(next_i as usize, removed);
+            }
+        }
+    }
+
+    coordinates(mixed)
 }
 
 fn main() {
@@ -68,7 +68,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 20);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(1623178306));
     }
 
     #[test]
@@ -76,6 +76,6 @@ mod tests {
     fn test_solutions() {
         let input = advent_of_code::read_file("inputs", 20);
         assert_eq!(part_one(&input), Some(8028));
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(8798438007673));
     }
 }
