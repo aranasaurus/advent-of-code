cf01f9e598e6168c6f5300f27026cb96b803ccef
Ryan Arana <ryan.arana@hey.com>
2024-12-03 23:58:59 -0800

    solve 2024 day 4 part 1 in rust

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -55,7 +55,194 @@ fn run(input_string: &str, day: u8, part: u8) -> Result<String, Error> {
 }
 
 fn day4(input_string: &str, part: u8) -> Result<String, Error> {
-    todo!()
+    match part {
+        1 => Ok(word_search(&input_string).to_string()),
+        2 => todo!(),
+        _ => Err(Error::InvalidPart),
+    }
+}
+
+fn word_search(input_string: &str) -> u32 {
+    let grid: Vec<Vec<usize>> = input_string
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    'X' => 1usize,
+                    'M' => 2usize,
+                    'A' => 3usize,
+                    'S' => 4usize,
+                    _ => 0usize,
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut result = 0u32;
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            if grid[y][x] == 1 {
+                result += score_x(&grid, x, y)
+            }
+        }
+    }
+    dbg!(result);
+    result
+}
+
+fn score_x(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> u32 {
+    let mut score = 0u32;
+    if check_n(&grid, x, y) {
+        score += 1
+    }
+    if check_ne(&grid, x, y) {
+        score += 1
+    }
+    if check_e(&grid, x, y) {
+        score += 1
+    }
+    if check_se(&grid, x, y) {
+        score += 1
+    }
+    if check_s(&grid, x, y) {
+        score += 1
+    }
+    if check_sw(&grid, x, y) {
+        score += 1
+    }
+    if check_w(&grid, x, y) {
+        score += 1
+    }
+    if check_nw(&grid, x, y) {
+        score += 1
+    }
+    score
+}
+
+const WORD_LEN: usize = 4;
+fn check_n(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> bool {
+    if y < WORD_LEN - 1 {
+        return false;
+    }
+
+    #[rustfmt::skip]
+    let word = vec![
+        grid[y][x],
+        grid[y - 1][x],
+        grid[y - 2][x],
+        grid[y - 3][x],
+    ];
+
+    word == vec![1, 2, 3, 4]
+}
+
+fn check_ne(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> bool {
+    if y < WORD_LEN - 1 || x > grid[y].len() - WORD_LEN {
+        return false;
+    }
+
+    let word = vec![
+        grid[y][x],
+        grid[y - 1][x + 1],
+        grid[y - 2][x + 2],
+        grid[y - 3][x + 3],
+    ];
+
+    word == vec![1, 2, 3, 4]
+}
+
+fn check_e(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> bool {
+    if x > grid[y].len() - WORD_LEN {
+        return false;
+    }
+
+    #[rustfmt::skip]
+    let word = vec![
+        grid[y][x],
+        grid[y][x + 1],
+        grid[y][x + 2],
+        grid[y][x + 3],
+    ];
+
+    word == vec![1, 2, 3, 4]
+}
+
+fn check_se(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> bool {
+    if y > grid.len() - WORD_LEN || x > grid[y].len() - WORD_LEN {
+        return false;
+    }
+
+    let word = vec![
+        grid[y][x],
+        grid[y + 1][x + 1],
+        grid[y + 2][x + 2],
+        grid[y + 3][x + 3],
+    ];
+
+    word == vec![1, 2, 3, 4]
+}
+
+fn check_s(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> bool {
+    if y > grid.len() - WORD_LEN {
+        return false;
+    }
+
+    #[rustfmt::skip]
+    let word = vec![
+        grid[y][x],
+        grid[y + 1][x],
+        grid[y + 2][x],
+        grid[y + 3][x],
+    ];
+
+    word == vec![1, 2, 3, 4]
+}
+
+fn check_sw(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> bool {
+    if x < WORD_LEN - 1 || y > grid.len() - WORD_LEN {
+        return false;
+    }
+
+    let word = vec![
+        grid[y][x],
+        grid[y + 1][x - 1],
+        grid[y + 2][x - 2],
+        grid[y + 3][x - 3],
+    ];
+
+    word == vec![1, 2, 3, 4]
+}
+
+fn check_w(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> bool {
+    if x < WORD_LEN - 1 {
+        return false;
+    }
+
+    #[rustfmt::skip]
+    let word = vec![
+        grid[y][x],
+        grid[y][x - 1],
+        grid[y][x - 2],
+        grid[y][x - 3],
+    ];
+
+    word == vec![1, 2, 3, 4]
+}
+
+fn check_nw(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> bool {
+    if x < WORD_LEN - 1 || y < WORD_LEN - 1 {
+        return false;
+    }
+
+    #[rustfmt::skip]
+    let word = vec![
+        grid[y][x],
+        grid[y - 1][x - 1],
+        grid[y - 2][x - 2],
+        grid[y - 3][x - 3],
+    ];
+
+    word == vec![1, 2, 3, 4]
 }
 
 fn day3(input_string: &str, part: u8) -> Result<String, Error> {
@@ -163,6 +350,27 @@ mod day3_tests {
     fn test_part2_input() -> io::Result<()> {
         let file = read_file("src/day3/input".to_string());
         assert_eq!("87163705", day3(&file, 2).unwrap());
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod day4_tests {
+    use std::io;
+
+    use super::*;
+
+    #[test]
+    fn test_part1_example() -> io::Result<()> {
+        let file = read_file("src/day4/example1".to_string());
+        assert_eq!("18", day4(&file, 1).unwrap());
+        Ok(())
+    }
+
+    #[test]
+    fn test_part1_input() -> io::Result<()> {
+        let file = read_file("src/day4/input".to_string());
+        assert_eq!("2547", day4(&file, 1).unwrap());
         Ok(())
     }
 }
